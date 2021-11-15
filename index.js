4f107ff75c883fe8107f760e1bb6efe8ba9ed202
Zohaib <zohaibmanzoor1700@gmail.com>
2021-11-15 15:16:54 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//write your code here
+const findLongestWord = string => {
+  let longest = "";
+  for (const i of string){
+    if (i.length > longest.length){
+      longest = i;
+    } 
+  }
+  return longest;
+};
+
+
+console.log(findLongestWord(["The","quick","brown", "fox", 
+"over", "the", "lazy", "dog", "jumpy" ]));
